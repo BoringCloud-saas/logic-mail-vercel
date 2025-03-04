b160a383d5006b4267c56da29c774d91d4638582
zdravkoCrypto <zdravko.jovanovic.tech@gmail.com>
2025-03-04 11:18:21 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function page() {
     return (
         <div className="flex flex-col h-screen w-full bg-[#fafafa]">
             <Navigation />
+            
         </div>
     )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+    env: {
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      AUTH_CLIENT_ID: process.env.AUTH_CLIENT_ID,
+      AUTH_CLIENT_SECRET: process.env.AUTH_CLIENT_SECRET,
+      AUTH_TENANT_ID: process.env.AUTH_TENANT_ID,
+    },
+};
+
 const nextConfig: NextConfig = {
-  /* config options here */
+
 };
 
 export default nextConfig;
